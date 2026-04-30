9bcf6265f640388766c976922dd9db74a4de4221
SHIVAM KUMAR SWARNKAR <sswarnkar0001@gmail.com>
2026-04-30 16:55:08 +0530

    Implement word vectorization using CountVectorizer
    Added CountVectorizer implementation with NLTK tokenizer to transform a corpus of sentences into a word vector representation.

---
+++ Text tasks/count_word_vectorize.py
@@ -0,0 +1,16 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import word_tokenize
+# create a corpus of sentences
+corpus = [
+ "hello, how are you?",
+ "im getting bored at home. And you? What do you think?",
+ "did you know about counts",
+ "let's see if this works!",
+ "YES!!!!"
+]
+# initialize CountVectorizer with word_tokenize from nltk
+# as the tokenizer
+ctv = CountVectorizer(tokenizer=word_tokenize, token_pattern=None)
+# fit the vectorizer on corpus
+ctv.fit(corpus)
+corpus_transformed = ctv.transform(corpus)
