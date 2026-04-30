e49788bb2a65e52220065523a76d364f817a9839
SHIVAM KUMAR SWARNKAR <sswarnkar0001@gmail.com>
2026-04-30 16:56:35 +0530

    Add output example for vocabulary mapping
    Added output example for the vocabulary mapping after fitting the vectorizer.

--- Text tasks/count_word_vectorize.py
+++ Text tasks/count_word_vectorize.py
@@ -15,3 +15,10 @@ ctv = CountVectorizer(tokenizer=word_tokenize, token_pattern=None)
 ctv.fit(corpus)
 corpus_transformed = ctv.transform(corpus)
 print(ctv.vocabulary_)
+
+# Output
+{'hello': 14, ',': 2, 'how': 16, 'are': 7, 'you': 27, '?': 4, 'im': 18,
+'getting': 13, 'bored': 9, 'at': 8, 'home': 15, '.': 3, 'and': 6, 'what':
+24, 'do': 12, 'think': 22, 'did': 11, 'know': 19, 'about': 5, 'counts':
+10, 'let': 20, "'s": 1, 'see': 21, 'if': 17, 'this': 23, 'works': 25,
+'!': 0, 'yes': 26}
